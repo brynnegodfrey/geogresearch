d4146f04f7300569b5d5097907da4d9e02b0bed7
Brynne Godfrey <bmgodfrey3@gmail.com>
2018-11-10 21:26:30 -0500

    changed

--- Turkey_data.R
+++ Turkey_data.R
@@ -7,7 +7,6 @@ library(plyr)
 library(rlang)
 library(dplyr)
 library(stringr)
-library(tidyverse)
 
 
 # Move all files to one list
@@ -45,43 +44,126 @@ weekdays <- weekdays(dates)
 
 #Add column to dataframe 
 out.file$weekdays <- weekdays
-STR_CALLERID <- toString(out.file$CALLER_ID)
-out.file$STR_CALLERID <- STR_CALLERID
 
-CALLER_CHAR <-as.character(out.file$CALLER_ID)
-STR_CALLERID %>%
-  group_by(grp = paste0('grp', substr(STR_CALLERID, 1, 1))) %>%
-  mutate(i = row_number()) %>%
-  spread(grp, STR_CALLERID) %>%
-  select(-i)
+#Group refugee CALLER_ID's together
+REFUGEE_BYSITE <- out.file[, c("SITE_ID", "CALLER_ID")]
+REFUGEE_BYSITE <- subset(REFUGEE_BYSITE, CALLER_ID == grep("^1.{9}", out.file$CALLER_ID, value=TRUE))
 
-STR_CALLERID  %>% 
-  group_by(grp = paste0('grp', substr(STR_CALLERID, 1, 1))) %>% 
-  mutate(i = row_number()) %>% 
-  spread(grp, STR_ID) %>%
-  select(-i)
-REFUGEE_CALLERS <- head(select(STR_CALLERID, starts_with("1")))
+COUNT_REFUGEES_BYSITE <- REFUGEE_BYSITE %>%
+  group_by(SITE_ID) %>%
+  summarise(n_distinct(CALLER_ID))
+colnames(COUNT_REFUGEES_BYSITE) <- c("SITE_ID", "REFUGEES")
+COUNT_REFUGEES_BYSITE <- COUNT_REFUGEES_BYSITE[-c(1), ]
+
+#Merge dataframes 
+length(COUNT_REFUGEES_BYSITE) = length(SITE_COUNT)
+DISTINCT_CALLERS <- merge(SITE_COUNT, COUNT_REFUGEES_BYSITE, by="SITE_ID")
+
+#Calculate Diversity Stat
+DIVERSITY_STAT <- DISTINCT_CALLERS$REFUGEES/DISTINCT_CALLERS$UNIQUE_CALLERS
+DISTINCT_CALLERS <- cbind(DISTINCT_CALLERS, DIVERSITY_STAT)
+summary_DIVERSITYSTAT <- summary(DIVERSITY_STAT)
+
 #Count frequency of calls per date
 CALLS_BYDATE <- out.file %>%
   group_by(weekdays) %>%
   summarise(n = n())
 colnames(CALLS_BYDATE) <- c("WEEKDAY", "#CALLS")
 
+#Clean data to include only refugee calls
+REFUGEECALLS_BYDATE <- out.file[, c("CALLER_ID","CALLEE_PREFIX", "weekdays")]
+REFUGEECALLS_BYDATE <- subset(REFUGEECALLS_BYDATE, CALLER_ID == grep("^1.{9}", out.file$CALLER_ID, value=TRUE))
+REFCALLS_BYDATE <- subset(REFUGEECALLS_BYDATE, CALLEE_PREFIX == "2")
+
+#Total Refugee Calls by Weekdays
+NewREFUGEECALLS_BYDATE <- REFUGEECALLS_BYDATE %>%
+  group_by(weekdays) %>%
+  summarise(n=n())
+colnames(NewREFUGEECALLS_BYDATE) <- c("Weekday", "Total_Refugee_Calls")
+
+#Total Refugee Calls to Natives by Weekdays
+REFCALLS_BYDATE <- REFCALLS_BYDATE %>%
+  group_by(weekdays) %>%
+  summarise(n=n())
+colnames(REFCALLS_BYDATE) <- c("Weekday", "Total_REFtoNATIVES")
+
+#Merge by weekday
+Percent_REFStoNATIVES <-merge(NewREFUGEECALLS_BYDATE, REFCALLS_BYDATE, by="Weekday")
+Percent_Calls_REFStoNATIVES <- Percent_REFStoNATIVES$Total_REFtoNATIVES/Percent_REFStoNATIVES$Total_Refugee_Calls
+REFtoNATIVE_BYDATE <- cbind(Percent_REFStoNATIVES, Percent_Calls_REFStoNATIVES)
+
+#Clean data to include only native calls
+NATIVECALLS_BYDATE <- out.file[, c("CALLER_ID","CALLEE_PREFIX", "weekdays")]
+NATIVECALLS_BYDATE <- subset(NATIVECALLS_BYDATE, CALLER_ID == grep("^2.{9}", out.file$CALLER_ID, value=TRUE))
+NATCALLS_BYDATE <- subset(NATIVECALLS_BYDATE, CALLEE_PREFIX == "1")
+
+#Total Native Calls by Weekdays
+NewNATIVECALLS_BYDATE <- NATIVECALLS_BYDATE %>%
+  group_by(weekdays) %>%
+  summarise(n=n())
+colnames(NewNATIVECALLS_BYDATE) <- c("Weekday", "Total_Native_Calls")
+
+#Total Native Calls to Refugees by Weekdays
+NATCALLS_BYDATE <- NATCALLS_BYDATE %>%
+  group_by(weekdays) %>%
+  summarise(n=n())
+colnames(NATCALLS_BYDATE) <- c("Weekday", "Total_NATtoREF")
+
+#Merge by weekday
+Percent_NATIVEStoREFS <-merge(NewNATIVECALLS_BYDATE, NATCALLS_BYDATE, by="Weekday")
+Percent_Calls_NATIVEStoREFS <- Percent_NATIVEStoREFS$Total_NATtoREF/Percent_NATIVEStoREFS$Total_Native_Calls
+REFtoNATIVE_BYDATE <- cbind(Percent_NATIVEStoREFS, Percent_Calls_NATIVEStoREFS)
+
 #Count unique sites/frequency
 SITE_COUNT <- out.file %>%
   group_by(SITE_ID) %>%
   summarise(n_distinct(CALLER_ID))
 colnames(SITE_COUNT) <- c("SITE_ID", "UNIQUE_CALLERS")
+SITE_COUNT <- SITE_COUNT[-c(1), ]
+
+
+REFUGEES_BYSITE <- REFUGEES %>%
+  group_by(SITE_ID) %>%
+  summarise(n_distinct(REFUGEES))
+colnames(REFUGEES_BYSITE) <- c("SITE_ID", "#REFUGEES")
 
 #Remove Outlier
 SITE_COUNT <- SITE_COUNT[-c(1), ]
 
 #Summary Stats
 summary_SITECOUNT <- summary(SITE_COUNT)
-toString(out.file$CALLER_ID)
-d <- out.file$CALLER_ID
-d %>% select(starts_with('1'))
 
+#Stats for each refugee caller to native
+UNIQUE_CALLER <- out.file[, c("CALLER_ID","CALLEE_PREFIX")]
+UNIQUE_CALLER <- subset(UNIQUE_CALLER, CALLER_ID == grep("^1.{9}", out.file$CALLER_ID, value=TRUE))
+UNIQUEtoNATIVE <- subset(UNIQUE_CALLER, CALLEE_PREFIX == "2")
+
+#Total Refugee Calls to Natives
+REFtoNATIVE <- UNIQUE_CALLER %>%
+  group_by(CALLER_ID) %>%
+  summarise(n=n())
+colnames(REFtoNATIVE) <- c("CALLER_ID", "NATIVE_CALLEE")
+
+#Stats for each refugee caller to unknown
+U_CALLER <- out.file[, c("CALLER_ID","CALLEE_PREFIX")]
+U_CALLER <- subset(U_CALLER, CALLER_ID == grep("^1.{9}", out.file$CALLER_ID, value=TRUE))
+U_CALLER <- subset(U_CALLER, CALLEE_PREFIX == "3")
+
+#Total Refugee Calls to Unknown
+REFtoU <- U_CALLER %>%
+  group_by(CALLER_ID) %>%
+  summarise(n=n())
+colnames(REFtoU) <- c("CALLER_ID", "UNKNOWN_CALLEE")
+
+#Merge columns (4)
+REF_CALLS <- merge(REFtoNATIVE, REFtoU, by="CALLER_ID")
+TOTAL_CALLS <- REF_CALLS$NATIVE_CALLEE + REF_CALLS$UNKNOWN_CALLEE
+REF_CALLS <- cbind(REF_CALLS, TOTAL_CALLS)
+
+#Percent calls each refugee places to natives
+Percent.REFtoNAT <- REF_CALLS$
+Percent_Calls_NATIVEStoREFS <- Percent_NATIVEStoREFS$Total_NATtoREF/Percent_NATIVEStoREFS$Total_Native_Calls
+REFtoNATIVE_BYDATE <- cbind(Percent_NATIVEStoREFS, Percent_Calls_NATIVEStoREFS)merge
 #Convert txt files to csv
 #files.to.read <- paste(directory, file_list, sep="/")
 #files.to.write <- paste(ndirectory, paste0(sub(".txt","", file_list), ".csv"), sep="")
